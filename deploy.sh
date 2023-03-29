4f00eca904b1b262c101d43a41280c3849c2241a
haoguoqiang <haoguoqiang@huami.com>
2023-03-29 09:44:59 +0800

    write  Wed Mar 29 09:44:59 CST 2023

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,16 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+
+git add .
+
+# Commit changes.
+msg="write $1 `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
